fce4cab7485e968abd8a63d26f776a8ecc316369
eldervaz <eldervaz@gmail.com>
2018-10-10 20:00:32 -0500

    edit text

--- streams.ts
+++ streams.ts
@@ -3,6 +3,9 @@ const fs = require("fs")
 const lectura = fs.createReadStream("./file.txt", "utf8")
 
 const escritura = fs.createWriteStream("./nuevo.txt")
+escritura.write('Hello world!\n');
+escritura.write('Another line\n');
+escritura.end();
 
 lectura.pipe(escritura)
 
