e7a83422119aa73298ab82ad503dc669b7aa0b5b
eldervaz <eldervaz@gmail.com>
2018-10-11 05:21:08 -0500

    toDo

--- to-to/app.js
+++ to-to/app.js
@@ -1,2 +1,27 @@
-const argv = require ("yargs").argv
+//const argv = require ("yargs").argv
+
+const argv = require (" ./config/yargs").argv
+
  console.log(argv)
+
+
+ let comando = argv._[0]
+
+ switch(comando){
+
+    case "create":
+        console.log("crear tarea")
+        break
+
+    case "list":
+        console.log("listar tareas")
+        break
+
+    case "update":
+        console.log("crear")
+        break
+
+        default:
+        console.log("Comando no diponible");
+
+ }
