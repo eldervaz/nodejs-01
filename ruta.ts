79ed5fe96c8e6511f6ad005a01f196a945c0cdb0
eldervaz <eldervaz@gmail.com>
2018-10-10 20:45:55 -0500

    rutas

--- ruta.ts
+++ ruta.ts
@@ -7,6 +7,10 @@ const s = http.createServer(
             res.writeHead(200, { "content-type": "text/html" })
             res.write("<h1>Bienvenido al servido ;)</h1>")
             res.end("<p>Estamos para servirte</p>")
+        }else{
+            res.writeHead(200, { "content-type": "text/html" })
+            res.write("<h1>¿estás perdido?</h1>")
+            res.end("<p>Esta página no está disponible</p>")
         }
     }
 )
