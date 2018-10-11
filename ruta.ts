f47d796797079b9979c2c425f9982ff0a5b074e4
eldervaz <eldervaz@gmail.com>
2018-10-10 20:30:54 -0500

    content-type html

--- ruta.ts
+++ ruta.ts
@@ -4,8 +4,9 @@ import http = require("http")
 const s = http.createServer(
     (req: http.IncomingMessage, res: http.ServerResponse) => {
         if (req.url === "/") {
-            res.writeHead(200, { "content-type": "text/plain" })
-            res.end("Bienvenido al servido ;)")
+            res.writeHead(200, { "content-type": "text/html" })
+            res.write("<h1>Bienvenido al servido ;)</h1>")
+            res.end("<p>Estamos para servirte</p>")
         }
     }
 )
