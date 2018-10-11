bf31cca9c8a1b03e0754fed8dcf5ed32e9ba70f1
eldervaz <eldervaz@gmail.com>
2018-10-10 19:43:13 -0500

    autoformat

--- ruta.ts
+++ ruta.ts
@@ -2,15 +2,14 @@ import http = require("http")
 
 
 const s = http.createServer(
-(req: http.IncomingMessage, res: http.ServerResponse)=>{
-if(req.url ==="/"){
-res.writeHead(200, {"content-type": "text/plain"})
-res.end("Bienvenido")
-}
-}
+    (req: http.IncomingMessage, res: http.ServerResponse) => {
+        if (req.url === "/") {
+            res.writeHead(200, { "content-type": "text/plain" })
+            res.end("Bienvenido")
+        }
+    }
 )
 
 
-
-s.listen(3000, ()=> console.log("Ejecutándose el servidor"))
+s.listen(3000, () => console.log("Ejecutándose el servidor"))
 
