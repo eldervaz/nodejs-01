dfd0ed37c86782ab24c88e485801dd536026b8c9
eldervaz <eldervaz@gmail.com>
2018-10-11 05:33:24 -0500

    yargs

--- to-to/config/yargs.js
+++ to-to/config/yargs.js
@@ -1,1 +1,29 @@
 const argv = require ("yargs")
+    .command("create", "crear nueva tarea", {
+        descripcion:{
+            demand:true,
+            alias: "d",
+            desc: "descripción de la tarea"
+        }
+    })
+
+    .command("update", "actualizar tarea", {
+        descripcion:{
+            demand:true,
+            alias: "d",
+            desc: "descripción de la tarea"
+        },
+        completado:{
+            default: true,
+            alias: "c",
+            desc: "Tarea completada"
+        }
+    })
+
+    .help()
+    .argv;
+
+
+ module.exports = {
+     argv
+ }   
