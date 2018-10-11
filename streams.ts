ea16a1a57965bb4d15d7cdea6643332b0950731c
eldervaz <eldervaz@gmail.com>
2018-10-10 20:03:57 -0500

    update file

--- streams.ts
+++ streams.ts
@@ -3,9 +3,12 @@ const fs = require("fs")
 const lectura = fs.createReadStream("./file.txt", "utf8")
 
 const escritura = fs.createWriteStream("./nuevo.txt")
-escritura.write('Hello world!\n');
-escritura.write('Another line\n');
-escritura.end();
+
+var data = "Hello, World!\n";
+escritura.write(data, function() {
+  // Now the data has been written.
+  console.log("update file");
+});
 
 lectura.pipe(escritura)
 
