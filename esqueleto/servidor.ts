efe65eca30e648b71a07acdfc8ef89300916d9cf
eldervaz <eldervaz@gmail.com>
2018-10-11 20:27:41 -0500

    esqueleto

--- esqueleto/servidor.ts
+++ esqueleto/servidor.ts
@@ -1,10 +1,15 @@
-import { Application } from "express"
+import { Application, Request, Response } from "express"
 
 import express = require("express")
 
 const app:Application = express()
 
-//app.get("/usuarios")
+app.get("/usuarios", (req:Request, res:Response)=>{
+    res
+        .json([
+            {nombre: "user1"}
+        ])
+})
 
 
 app.listen(3000, ()=>console.log("Servidor puerto 3000"))
